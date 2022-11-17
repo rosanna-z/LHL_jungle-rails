41b55380e86cc2b241d24c4fe5ac2f9e5eb9c49f
Rosanna Zhang <rosannazhang92@gmail.com>
2022-11-17 00:16:30 -0800

    Add table that counts the total number of products and categories - also links the product/category page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+  #Display a count of how many products are in the database
+  #Display a count of how many categories are in the database
+  
   def show
+    @totalproducts = Product.count
+    @totalcategories = Category.count
   end
+
 end
